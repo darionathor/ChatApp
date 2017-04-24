25151a80081a7ebfa4bd6b5ff90e377756bbbd3d
Sasa Lalic <sasalali@gmail.com>
2017-04-24 21:02:27 +0200

    Rest endpoints

--- User/ejbModule/beans/UserApp.java
+++ User/ejbModule/beans/UserApp.java
@@ -1,9 +1,20 @@
 package beans;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ejb.ActivationConfigProperty;
 import javax.ejb.MessageDriven;
 import javax.jms.Message;
 import javax.jms.MessageListener;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import exceptions.InvalidCredentialsException;
+import exceptions.UsernameExistsException;
 
 /**
  * Message-Driven Bean implementation class for: User
@@ -13,12 +24,13 @@ import javax.jms.MessageListener;
 				propertyName = "destinationType", propertyValue = "javax.jms.Queue")
 		})
 public class UserApp implements MessageListener {
-
+	private ArrayList<User> registeredUsers;
     /**
      * Default constructor. 
      */
     public UserApp() {
         // TODO Auto-generated constructor stub
+    	registeredUsers=new ArrayList<User>();
     }
 	
 	/**
@@ -28,5 +40,52 @@ public class UserApp implements MessageListener {
         // TODO Auto-generated method stub
         
     }
+    @POST
+    @Path("/register")
+    @Produces(MediaType.APPLICATION_JSON)
+    User register(String username, String password) throws UsernameExistsException {
+    	for(User us: registeredUsers){
+			if(us.getUsername().equals(username)){
+				throw new UsernameExistsException();
+			}
+		}
+    	User user= new User();
+    	user.setPassword(password);
+    	user.setUsername(username);
+    	user.setHost(null);
+    	registeredUsers.add(user);
+    	return user;
+	}
 
+    @POST
+    @Path("/login")
+    @Produces(MediaType.APPLICATION_JSON)
+    Boolean login(String username, String password) throws InvalidCredentialsException {
+    	for(User us: registeredUsers){
+			if(us.getUsername().equals(username) && us.getPassword().equals(password)){
+				return true;
+			}
+		}
+    	return false;
+	}
+
+    @POST
+    @Path("/logout")
+    @Produces(MediaType.APPLICATION_JSON)
+    Boolean logout(User logout) {
+    	for(User us: registeredUsers){
+			if(us.getUsername().equals(logout.getUsername()) && us.getPassword().equals(logout.getPassword())){
+				us.setHost(null);
+				return true;
+			}
+		}
+		return false;
+	}
+
+    @GET
+    @Path("/registeredUsers")
+    @Produces(MediaType.APPLICATION_JSON)
+    List<User> getAllUsers() {
+		return registeredUsers;
+	}
 }
